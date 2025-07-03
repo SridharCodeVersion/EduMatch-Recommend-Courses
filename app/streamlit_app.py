ae4d98c8dd3e36a15a0dfb3713ae574e629054ea
SridharCodeVersion <iyernman2025@gmail.com>
2025-07-03 16:03:26 +0530

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 from main import recommend
 
+# Add required downloads for Streamlit Cloud
+import nltk
+import spacy
+nltk.download('punkt')
+nltk.download('stopwords')
+spacy.cli.download("en_core_web_sm")
+
 st.title("🎓 EduMatch: Course Recommender")
 
 resume = st.text_area("Paste your resume or skills:")
